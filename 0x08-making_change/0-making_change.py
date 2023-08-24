c47ca0be1bb8e0197b6fdd51bc72a8dfe1d3a331
Precious Idubor <preciousidubor@gmail.com>
2023-08-24 13:17:15 -0700

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    """Determine the fewest number of coins needed
+    to meet a given amount total"""
+    if total <= 0:
+        return 0
+
+    coins = sorted(coins, reverse=True)
+    num = 0
+    for coin in coins:
+        if coin <= total:
+            num += total // coin
+            total %= coin
+            if total == 0:
+                return num
+
+    return -1
